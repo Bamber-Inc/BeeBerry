d0a9b85f91f04528e81ee352eb481bb2a3011b2c
Bryan Bamber <bryan.bamber@kurtosys.com>
2019-02-24 18:51:25 +0200

    git update

---
+++ nodejs-picam2/app.js
@@ -0,0 +1,24 @@
+var RaspiCam = require("raspicam");
+
+var camera = new RaspiCam({
+	mode: "photo",
+	output: "/data/image.jpg",
+	encoding: "jpg",
+	timeout: 100
+});
+
+camera.on("started", function( err, timestamp ){
+	console.log("photo started at " + timestamp );
+});
+
+
+camera.on("read", function( err, timestamp, filename ){
+	console.log("photo image captured with filename: " + filename );
+	//we can now do stuff with the captured image, which is stored in /data
+});
+
+camera.on("exit", function( timestamp ){
+	console.log("photo child process has exited at " + timestamp );
+});
+
+camera.start();
