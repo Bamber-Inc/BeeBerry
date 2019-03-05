b46a84d9971e999125c28595b74f3a4e02eb0de2
Bryan Bamber <bryan.bamber@kurtosys.com>
2019-03-05 14:00:20 +0200

    changed to video

--- nodejs-picam/app.js
+++ nodejs-picam/app.js
@@ -1,14 +1,36 @@
 var RaspiCam = require("raspicam");
 
+// Photo configuration
+var camera = new RaspiCam({
+	mode: "photo",
+	output: "/data/image001.jpg",
+	encoding: "jpg",
+	ex: "auto",
+	rot: "180",
+});
+
+// Timelapse configuration
 var camera = new RaspiCam({
 	mode: "timelapse",
-	output: "/data/image%04d.jpg",
+	output: "/data/image%03d.jpg",
 	encoding: "jpg",
 	tl: "1500",
 	ex: "auto",
 	rot: "180",
 	t: "60000"
 });
+
+// Video configuration - bitrate 0.1Mbits/s with timeout at 60s with 10 frames per second
+var camera = new RaspiCam({
+	mode: "video",
+	output: "/data/video001.mp4",
+	b: "100000",
+	t: "6000",
+	fps: "10",
+	ex: "auto",
+	rot: "180",
+});
+
 
 //listen for the "start" event triggered when the start method has been successfully initiated
 camera.on("started", function( err, timestamp ){
@@ -32,10 +54,10 @@ camera.start();
 // loads of more options below
 
 //to stop a timelapse or video recording
-// camera.stop( );
+camera.stop();
 
 
 //listen for the "stop" event triggered when the stop method was called
 camera.on("stop", function( err, timestamp ){
-	console.log("photo started at " + timestamp );
+	console.log("capture started at " + timestamp );
 });
