91d00bc537bdfd7c244db7f53b95c74ebdcce1c2
Bryan Bamber <bryan.bamber@kurtosys.com>
2019-03-05 14:28:16 +0200

    syntax and clean up examples

--- nodejs-picam/app.js
+++ nodejs-picam/app.js
@@ -1,35 +1,34 @@
 var RaspiCam = require("raspicam");
 
 // Photo configuration
-var camera = new RaspiCam({
-	mode: "photo",
-	output: "/data/image001.jpg",
-	encoding: "jpg",
-	ex: "auto",
-	rot: "180",
-});
+//var camera = new RaspiCam({
+//	mode: "photo",
+//	output: "/data/image001.jpg",
+//	encoding: "jpg",
+//	ex: "auto",
+//	rot: "180",
+//});
 
 // Timelapse configuration
-var camera = new RaspiCam({
-	mode: "timelapse",
-	output: "/data/image%03d.jpg",
-	encoding: "jpg",
-	tl: "1500",
-	ex: "auto",
-	rot: "180",
-	t: "60000"
-});
+//var camera = new RaspiCam({
+//	mode: "timelapse",
+//	output: "/data/image%03d.jpg",
+//	encoding: "jpg",
+//	tl: "1500",
+//	ex: "auto",
+//	rot: "180",
+//	t: "60000"
+//});
 
 // Video configuration - bitrate 0.1Mbits/s with timeout at 60s with 10 frames per second
 var camera = new RaspiCam({
 	mode: "video",
 	output: "/data/video001.mp4",
-	t: "6000",
+	t: "60000",
 	fps: "10",
 	ex: "auto",
 	rot: "180",
 });
-
 
 //listen for the "start" event triggered when the start method has been successfully initiated
 camera.on("started", function( err, timestamp ){
@@ -53,7 +52,7 @@ camera.start();
 // loads of more options below
 
 //to stop a timelapse or video recording
-camera.stop();
+//camera.stop();
 
 
 //listen for the "stop" event triggered when the stop method was called
